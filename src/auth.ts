6cfb5e7732f4cda347e6686e16f61497e079b29b
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-08-24 10:08:52 +0100

    adds google

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+
 import { db } from "./db"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [GitHub],
+  providers: [
+    GitHub,
+    Google
+  ],
 })
