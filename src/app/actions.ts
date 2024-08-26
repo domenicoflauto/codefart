62821d81141c94b011d0adccae241e33a7222f83
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-08-26 18:28:44 +0100

    fix error on createSnippets action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,17 +2,20 @@
 
 import { db } from "@/db"
 import { snippets } from "@/db/schema/snippets";
+import { auth } from "@/auth";
 
 import { generateRandomString } from "@/utils";
 
 export async function createSnippet() {
   // user auth check
+  const session = await auth()
   const id = generateRandomString(16)
   
   await db.insert(snippets).values({
     id: id,
     name: id,
-    content: "a brand new snippet"
+    content: "a brand new snippet",
+    user: session?.user?.id!
   })
 
   return {
