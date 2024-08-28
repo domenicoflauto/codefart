f6a9502b067adf19e71a6f3648989b96f519e4c5
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-08-28 22:01:53 +0100

    adds ability to remove snippets

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,12 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+
 import { db } from "@/db"
 import { snippets } from "@/db/schema/snippets";
 import { auth } from "@/auth";
-
 import { generateRandomString } from "@/utils";
+import { eq } from "drizzle-orm";
 
 export async function createSnippet(text: string) {
   // user auth check
@@ -26,4 +28,9 @@ export async function createSnippet(text: string) {
 export async function getSnippets() {
   const allSnippets = await db.select().from(snippets)
   return allSnippets
+}
+
+export async function deleteSnippet(id: string) {
+  await db.delete(snippets).where(eq(snippets.id, id))
+  revalidatePath("/")
 }
--- src/components/allSnippets.tsx
+++ src/components/allSnippets.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { deleteSnippet } from "@/app/actions";
 import { useEffect, useState } from "react";
 
 type snippet = {
@@ -8,18 +9,36 @@ type snippet = {
   content: string;
   visibility: "private" | "public";
   createdAt: string;
-} | undefined;
+};
+
+interface AllSnippetsProps {
+  snippets: snippet[];
+}
+
+interface SnippetProps {
+  snippet: snippet;
+  deleteSnippet: (id: string) => void;
+}
 
 
-export default function AllSnippets({ snippets }: { snippets: snippet[] }) {
+export default function AllSnippets({
+  snippets,
+}: AllSnippetsProps) {
   const [snippetItems, setSnippetItems] = useState<snippet[]>(snippets!);
 
   useEffect(() => {
     setSnippetItems(snippets);
   }, [snippets]);
 
-  const handleAddSnippet = (newSnippet: snippet) => {
-    setSnippetItems([...snippetItems, newSnippet]);
+  // const handleAddSnippet = (newSnippet: snippet) => {
+  //   setSnippetItems([...snippetItems, newSnippet]);
+  // };
+
+
+  // Function to delete a todo item
+  const deleteSnippetItem = (id: string) => {
+    setSnippetItems((prev) => prev.filter((snippet) => snippet.id !== id));
+    deleteSnippet(id);
   };
 
   return (
@@ -27,12 +46,30 @@ export default function AllSnippets({ snippets }: { snippets: snippet[] }) {
       <div className="w-full flex flex-col mt-8 gap-2">
         Snippets:
         {snippetItems.map((snippet) => (
-          <div key={snippet?.id} className="flex flex-row justify-between items-center p-4 rounded-md">
-            <div className="text-sm">{snippet?.content}</div>
-            <div className="text-sm">{snippet?.createdAt}</div>
-          </div>
+          <Snippet key={snippet.id} snippet={snippet} deleteSnippet={deleteSnippetItem} />
         ))}
       </div>
     </>
   );
+}
+
+export function Snippet({
+  snippet,
+  deleteSnippet
+}: SnippetProps) {
+
+  // Event handler for deleting a todo item
+  const handleDelete = () => {
+    if (confirm("Are you sure you want to delete this todo?")) {
+      deleteSnippet(snippet.id);
+    }
+  };
+
+  return (
+    <div key={snippet?.id} className="flex flex-row justify-between items-center p-4 rounded-md">
+      <div className="text-sm">{snippet?.content}</div>
+      <div className="text-sm">{snippet?.createdAt}</div>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  )
 }
