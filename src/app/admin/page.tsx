32040198a03e6041f5e43a3ba4aae3afb95f061e
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-09-03 22:12:57 +0100

    playing around with admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import ConfirmSomething from "./components/ConfirmSomething";
 
 export default async function Page() {
   const session = await auth()
@@ -8,13 +9,22 @@ export default async function Page() {
     isAdmin
       ? (
         <div>
-          <Tabs defaultValue="account" className="w-[400px]">
+
+          <Tabs defaultValue="account" className="w-[400px] m-auto">
             <TabsList>
               <TabsTrigger value="account">Account</TabsTrigger>
               <TabsTrigger value="password">Password</TabsTrigger>
+              <TabsTrigger value="somethingelse">Something else</TabsTrigger>
             </TabsList>
-            <TabsContent value="account">Make changes to your account here.</TabsContent>
+            <TabsContent value="account">
+              <div className="flex flex-col bg-slate-500">
+                Make changes to your account here.
+                <ConfirmSomething />
+
+              </div>
+            </TabsContent>
             <TabsContent value="password">Change your password here.</TabsContent>
+            <TabsContent value="somethingelse">Something else....</TabsContent>
           </Tabs>
 
         </div >
