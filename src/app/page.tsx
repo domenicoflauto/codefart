c2c0ca446a3b70b81464588a50f82d8648adb2ba
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-09-05 22:33:27 +0100

    adds gradient to text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,13 +28,20 @@ export default async function Home() {
     <main className="w-full  flex min-h-screen flex-col justify-between items-center p-24">
 
       <Flex direction="column" gap="2">
-        <Text>Hello from Radix Themes :)</Text>
-        <Button>Let&apos;s go</Button>
+        <Text className="text-3xl font-semibold text-indigo-500">
+          <span className="dark:bg-gradient-to-r dark:from-indigo-600 dark:to-sky-400 dark:text-transparent dark:bg-clip-text">
+            Experiments
+          </span>
+        </Text>
+        <Text as="p" className="text-2xl/6 tracking-tight text-zinc-950 dark:text-zinc-50">
+          Using tailwindcss, radix-ui, and other cool stuff.
+        </Text>
       </Flex>
 
-      <div className="flex flex-col justify-center items-center">
-        <p>Homepage of experiments, trash, etc.</p>
-        <br />
+
+
+      <div className="mt-12 flex flex-col justify-center items-center">
+
         <CodeExample code={codeString} language="html" fileName="snippet 1" />
 
         {snippets && <AllSnippets session={session} snippets={snippets} />}
