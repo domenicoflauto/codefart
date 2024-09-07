f3d94dd5de8d5eb96db9a231fbbdb10db333e4f0
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-09-07 10:57:40 +0100

    adds AI

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,6 +8,41 @@ import { auth } from "@/auth";
 import { eq } from "drizzle-orm";
 import { users } from "@/db/schema/users";
 
+import { streamText } from 'ai';
+import { openai } from '@ai-sdk/openai';
+import { createStreamableValue } from 'ai/rsc';
+
+export interface Message {
+  role: 'user' | 'assistant';
+  content: string;
+}
+
+export async function continueConversation(history: Message[]) {
+  'use server';
+
+  const stream = createStreamableValue();
+
+  (async () => {
+    const { textStream } = await streamText({
+      model: openai('gpt-3.5-turbo'),
+      system:
+        "You are a dude that doesn't drop character until the DVD commentary.",
+      messages: history,
+    });
+
+    for await (const text of textStream) {
+      stream.update(text);
+    }
+
+    stream.done();
+  })();
+
+  return {
+    messages: history,
+    newMessage: stream.value,
+  };
+}
+
 export async function createSnippet(text: string, id: string, language: string) {
   // user auth check
   const session = await auth()
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,36 @@
-import CodeExample from "@/components/CodeExample";
-import { auth } from "@/auth";
+import { Flex, Text } from '@radix-ui/themes';
 
-
-import AllSnippets from "@/components/allSnippets";
-import { getSnippets } from "./actions";
-
-import { Flex, Text, Container } from '@radix-ui/themes';
+import { Chat } from '@/components/Chat';
+import { auth } from '@/auth';
 
 export default async function Home() {
-  const snippets = await getSnippets()
-
-  const session = await auth()
-
-  const codeString = `<style>
-    button {
-      background-color: #111111;
-      color: #F5F5F5;
-      padding: 8px 12px;
-      border-radius: 6px;
-    }
-    </style>
-    <body>
-      <button>Click me</button>
-    </body>`;
-
+  const session = await auth();
   return (
-    <main className="w-full  flex min-h-screen flex-col justify-between items-start">
-      <Flex direction="column" gap="2">
-        <Text className="text-sm font-semibold text-indigo-500">
-          <span className="dark:bg-gradient-to-r dark:from-indigo-600 dark:to-sky-400 dark:text-transparent dark:bg-clip-text">
-            Experiments
-          </span>
-        </Text>
-        <Text as="p" className="text-2xl/6 tracking-tight text-zinc-950 dark:text-zinc-50">
-          Using tailwindcss, radix-ui, and other cool stuff.
-        </Text>
-      </Flex>
+    <main className="w-full flex min-h-screen flex-col justify-between items-start">
+      <Flex direction="column" gap="4">
+        <Flex direction="column" gap="2">
+          <Text className="text-sm font-semibold text-indigo-500">
+            <span className="dark:bg-gradient-to-r dark:from-indigo-600 dark:to-sky-400 dark:text-transparent dark:bg-clip-text">
+              Experiments
+            </span>
+          </Text>
+          <Text as="p" className="text-2xl/6 tracking-tight text-zinc-950 dark:text-zinc-50">
+            Using tailwindcss, radix-ui, and other cool stuff.
+          </Text>
+        </Flex>
 
-      <Flex direction="column" gap="2" className="mt-6">
-        <Text className="text-sm font-semibold text-indigo-500">
-          <span className="dark:bg-gradient-to-r dark:from-indigo-600 dark:to-sky-400 dark:text-transparent dark:bg-clip-text">
-            Can-do attitude
-          </span>
-        </Text>
-        <Text as="p" className="text-2xl/6 tracking-tight text-zinc-950 dark:text-zinc-50">
-          I don&apos;t care about this website
-        </Text>
+        <Flex direction="column" gap="2" className="mt-6">
+          <Text className="text-sm font-semibold text-indigo-500">
+            <span className="dark:bg-gradient-to-r dark:from-indigo-600 dark:to-sky-400 dark:text-transparent dark:bg-clip-text">
+              Can-do attitude
+            </span>
+          </Text>
+          <Text as="p" className="text-2xl/6 tracking-tight text-zinc-950 dark:text-zinc-50">
+            I don&apos;t care about this website
+          </Text>
+        </Flex>
       </Flex>
-
-      <div className="mt-12 flex flex-col justify-center items-center w-full">
-        <CodeExample code={codeString} language="html" fileName="snippet 1" />
-        {snippets && <AllSnippets session={session} snippets={snippets} />}
-      </div>
+      {session?.user?.role === 'admin' && <Chat />}
       <span className="text-xs opacity-35">© 2024 - All rights reserved</span>
     </main>
   );
