858cf7d38b327e72560bd5f748481a6757fbbdcb
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-09-17 22:44:40 +0100

    adds clerk

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,13 +1,7 @@
 "use server"
 
-import { revalidatePath } from "next/cache";
-
 import { db } from "@/db"
-import { snippets } from "@/db/schema/snippets";
 import { transactions, tags } from "@/db/schema/transactions";
-import { auth } from "@/auth";
-import { eq } from "drizzle-orm";
-import { users } from "@/db/schema/users";
 
 import { streamText } from 'ai';
 import { openai } from '@ai-sdk/openai';
@@ -74,40 +68,4 @@ export async function createTag(tag: string) {
   return {
     tag
   }
-}
-
-export async function createSnippet(text: string, id: string, language: string) {
-  // user auth check
-  const session = await auth()
-  
-  await db.insert(snippets).values({
-    id: id,
-    name: id,
-    content: text,
-    user: session?.user?.id!,
-    language: language
-  })
-
-  return {
-    id
-  }
-}
-
-export async function getSnippets() {
-  const allSnippets = await db.select({
-    userId: users.id,
-    id: snippets.id,
-    name: snippets.name,
-    content: snippets.content,
-    visibility: snippets.visibility,
-    createdAt: snippets.createdAt,
-    userName: users.name,
-    language: snippets.language
-  }).from(snippets).leftJoin(users, eq(snippets.user, users.id))
-  return allSnippets
-}
-
-export async function deleteSnippet(id: string) {
-  await db.delete(snippets).where(eq(snippets.id, id))
-  revalidatePath("/")
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,15 @@ import { cn } from "@/lib/utils"
 import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import '@radix-ui/themes/styles.css';
-import { TopBar } from "@/components/TopBar";
-import { auth, signIn, signOut } from "@/auth";
+
+import {
+  ClerkProvider,
+  UserButton,
+} from '@clerk/nextjs'
+
+import { currentUser } from '@clerk/nextjs/server'
 
 import { ThemeProvider } from "@/components/providers"
-
-
 import UmamiProvider from 'next-umami'
 
 const fontSans = FontSans({
@@ -26,47 +29,37 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const session = await auth()
-  const handleLogin = async () => {
-    "use server"
-    await signIn()
-  }
-
-  const handleLogout = async () => {
-    "use server"
-    await signOut()
-  }
+  const user = await currentUser()
 
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <UmamiProvider websiteId="30922084-df7f-48c8-a600-55fad18ed1f1" />
-      </head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <UmamiProvider websiteId="30922084-df7f-48c8-a600-55fad18ed1f1" />
+        </head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
         >
-          <TopBar
-            session={session}
-            login={handleLogin}
-            logout={handleLogout}
-          />
-          <div className="container pt-16">
-            {children}
-          </div>
-        </ThemeProvider>
-      </body>
-      <Analytics />
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="container pt-16">
+              <UserButton />
+              {children}
+            </div>
+          </ThemeProvider>
+        </body>
+        <Analytics />
+      </html>
+    </ClerkProvider>
+
   );
 }
