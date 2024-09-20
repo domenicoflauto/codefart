43fabfa3061c5bcee58fc3815f039bbad3faf80b
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-09-21 00:02:12 +0100

    style signup form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,20 +1,39 @@
 "use server"
 
 import { signup } from "./actions";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
 
 export default async function Page() {
   return (
-    <>
-      <h1>Create an account</h1>
-      <form action={signup}>
-        <label htmlFor="username">Username</label>
-        <input name="username" id="username" />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <br />
-        <button>Continue</button>
-      </form>
-    </>
+    <div className="flex items-center justify-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Sign up</CardTitle>
+          <CardDescription>Create an account</CardDescription>
+        </CardHeader>
+        <form action={signup}>
+          <CardContent>
+            <Label htmlFor="username">Username</Label>
+            <Input name="username" id="username" />
+            <Label htmlFor="password">Password</Label>
+            <Input type="password" name="password" id="password" />
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Continue</Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
   );
 }
